33b239fd8e9c9ccaee8bbe6396f7ef7d5ef96d2c
Matthias LÃuecke <mattisluecke@gmail.com>
2020-11-17 17:53:53 +0100

    added controller fixes to access index

--- app/controllers/vans_controller.rb
+++ app/controllers/vans_controller.rb
@@ -1,29 +1,43 @@
 class VansController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
   def index
     @vans  = policy_scope(Van).order(created_at: :desc)
   end
-
-  def new
-    # authorize @van
-  end
-  
-  def create
-    # authorize @van
-  end
-  
-  def update
-    # authorize @van
-  end
-  
-  def edit
-    # authorize @van
-  end
-  
-  def destroy
-    # authorize @van
-  end
   
   def show
-    # authorize @van
+    @van = Van.find(params[:id])
+    authorize @van
   end
+
+  def new
+    @van = Van.new
+    authorize @van
+  end
+  
+  # def create
+  #   # @van = Van.new(strong_params)
+  #   # authorize @van
+  #   # @van.user = current_user
+  #   # if @van.save....
+  # end
+  
+  # def update
+  #   # authorize @van
+  # end
+  
+  # def edit
+  #   # authorize @van
+  # end
+  
+  # def destroy
+  #   # authorize @van
+  # end
+  
+  # private
+
+  # def strong_params
+  # end
+
+
+
 end
--- app/policies/van_policy.rb
+++ app/policies/van_policy.rb
@@ -3,5 +3,34 @@ class VanPolicy < ApplicationPolicy
     def resolve
       scope.all
     end
+    
   end
+  
+  def show?
+    true
+  end
+
+  def new?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    true
+  end
+
+  def edit?
+    true
+  end
+
+  def destroy?
+    true
+  end
+
+  
+
+
 end
