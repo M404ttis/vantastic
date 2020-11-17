211d77efc3c8b06670ac33858f6826fce65f532f
Witta Glöckner <alina.glocke@gmail.com>
2020-11-17 12:14:09 +0100

    created seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+puts "Start seeding user..."
+
+100.times do
+  User.create!(
+  username: Faker::Name.name,
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name
+    )
+end
+
+puts "End seeding user, #{User.count} created ..."
+
+puts "Start seeding van..."
+
+30.times do
+  faker_brand = Faker::Vehicle.make
+  Van.create!(
+    title: Faker::Marketing.buzzwords,
+    brand: faker_brand,
+    model: Faker::Vehicle.model(make_of_model: faker_brand),
+    description: Faker::Lorem.paragraphs(number: 1),
+    photo: Faker::LoremFlickr.image(size: "50x60", search_terms: [faker_brand]),
+    price_per_day: rand(20..1000),
+    user: User.all.sample
+    )
+end
+
+puts "End seeding van, #{Van.count} created ..."
