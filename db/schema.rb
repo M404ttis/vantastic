ea39998f7fe4952d420e6df73d2bd90a1378fae0
lennsen <lennjen@gmail.com>
2020-11-19 12:23:35 +0100

    created a migration to change the default status of 'confirmed' to 'false'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_19_110515) do
+ActiveRecord::Schema.define(version: 2020_11_19_111837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2020_11_19_110515) do
   end
 
   create_table "bookings", force: :cascade do |t|
-    t.boolean "confirmed"
+    t.boolean "confirmed", default: false
     t.bigint "user_id", null: false
     t.bigint "van_id", null: false
     t.datetime "created_at", precision: 6, null: false
