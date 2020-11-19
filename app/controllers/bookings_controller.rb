8023ce7cd3e56e084056c7be5596ae9d4be7a455
Leif Sehlbach <leif.sehlbach@gmail.com>
2020-11-19 14:05:33 +0100

    bookings show created

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,27 +1,28 @@
 class BookingsController < ApplicationController
 
 
-  
+
   def show
-    # authorize @booking
+    @booking = Booking.find(params[:id])
+    authorize @booking
   end
-  
+
   def new
     # authorize @booking
   end
-  
+
   def create
     # authorize @booking
   end
-  
+
   def edit
     # authorize @booking
   end
-  
+
   def update
     # authorize @booking
   end
-  
+
   def destroy
     # authorize @booking
   end
